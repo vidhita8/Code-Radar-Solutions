a2dd9269a3cc0f6bbd6c19b9c701a3e956cf654f
vidhita8 <guptavidhita@gmail.com>
2025-02-13 23:19:12 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,28 +1,65 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int a,b;
+//     char c;
+//     scanf("%d %d %c", &a, &b, &c);
+//     if (c=='+')
+//     {
+//         printf("%d", a+b);
+//     }
+//     else if (c=='-')
+//     {
+//         printf("%d", a-b);
+//     }
+//     else if (c=='*')
+//     {
+//         printf("%%.2f", a*b);
+//     }
+//     else if (c=='/')
+//     {
+//         printf("%.2f", a/b);
+//     }
+//     else
+//     {
+//         printf("Invalid grade");
+//     }
+//     return 0;
+// }
 #include <stdio.h>
 int main()
 {
-    int a,b;
+    int a, b;
     char c;
-    scanf("%d %d %c", &a, &b, &c);
-    if (c=='+')
+    scanf("%d %d %c", &a, &b, &c); // Input two integers and an operator
+    
+    if (c == '+')
     {
-        printf("%d", a+b);
+        printf("%d", a + b);  // Addition
     }
-    else if (c=='-')
+    else if (c == '-')
     {
-        printf("%d", a-b);
+        printf("%d", a - b);  // Subtraction
     }
-    else if (c=='*')
+    else if (c == '*')
     {
-        printf("%%.2f", a*b);
+        printf("%.2f", (float)(a * b));  // Multiplication (output as float)
     }
-    else if (c=='/')
+    else if (c == '/')
     {
-        printf("%.2f", a/b);
+        if (b != 0)  // To prevent division by zero
+        {
+            printf("%.2f", (float)a / b);  // Division (output as float)
+        }
+        else
+        {
+            printf("Division by zero error");  // Handle division by zero
+        }
     }
     else
     {
-        printf("Invalid grade");
+        printf("Invalid operator");  // Handle invalid operator
     }
+    
     return 0;
 }
