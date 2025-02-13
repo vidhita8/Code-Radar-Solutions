a6c85097e9344b27e6195cacd5095292f23fc4f6
vidhita8 <guptavidhita@gmail.com>
2025-02-13 23:14:00 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,24 +4,25 @@ int main()
     int a,b;
     char c;
     scanf("%d %d %c", &a, &b, &c);
-    if (c=='+')
-    {
-        printf("%d", a+b);
-    }
-    else if (c=='-')
-    {
-        printf("%d", a-b);
-    }
-    else if (c=='*')
-    {
-        printf("%%.2lb", a*b);
-    }
-    else if (c=='/')
-    {
-        printf("%.2lb", a/b);
-    }
-    else
-    {
-        printf("Invalid grade");
-    }
+    // if (c=='+')
+    // {
+    //     printf("%d", a+b);
+    // }
+    // else if (c=='-')
+    // {
+    //     printf("%d", a-b);
+    // }
+    // else if (c=='*')
+    // {
+    //     printf("%%.2lb", a*b);
+    // }
+    // else if (c=='/')
+    // {
+    //     printf("%.2lb", a/b);
+    // }
+    // else
+    // {
+    //     printf("Invalid grade");
+    // }
+    print("0")
 }
