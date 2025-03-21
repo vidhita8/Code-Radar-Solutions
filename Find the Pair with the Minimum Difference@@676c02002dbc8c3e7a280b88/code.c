4ca1206d142963f632b8991e74773ffecba6e2a2
vidhita8 <guptavidhita@gmail.com>
2025-03-21 21:49:48 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,37 +1,87 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main()
+// {
+//     int n, diff=100000, first=0,second=0;
+//     scanf("%d", &n);
+//     int arr[n];
+//     if (n>1)
+//     {
+//     for (int i=0; i<n; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
+//     for(int i=0; i<n; i++)
+//     {
+//         for(int j=i+1; j<n; j++)
+//         {
+//             if (abs(arr[i]-arr[j])<diff)
+//             diff=abs(arr[i]-arr[j]);
+//             first=arr[i];
+//             second=arr[j];
+//         }
+//     }
+//     if (first>second)
+//     {
+//         printf("%d %d",second, first);
+//     }
+//     else if (second>first)
+//     {
+//         printf("%d %d",first, second);
+//     }
+//     }
+//     else
+//     {
+//         printf("-1");
+//     }
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
+
 int main()
 {
-    int n, diff=100000, first=0,second=0;
+    int n, diff = 100000, first = 0, second = 0;
     scanf("%d", &n);
-    int arr[n];
-    if (n>1)
+
+    if (n <= 1) // If there's 1 or fewer elements, print -1
     {
-    for (int i=0; i<n; i++)
+        printf("-1");
+        return 0;
+    }
+
+    int arr[n];
+
+    // Read the array elements
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
-    for(int i=0; i<n; i++)
+
+    // Find the pair with the smallest absolute difference
+    for (int i = 0; i < n - 1; i++) // Loop through each element
     {
-        for(int j=i+1; j<n; j++)
+        for (int j = i + 1; j < n; j++) // Compare with each subsequent element
         {
-            if (abs(arr[i]-arr[j])<diff)
-            diff=abs(arr[i]-arr[j]);
-            first=arr[i];
-            second=arr[j];
+            int current_diff = abs(arr[i] - arr[j]); // Calculate absolute difference
+            if (current_diff < diff) // If this pair has a smaller difference
+            {
+                diff = current_diff; // Update the smallest difference
+                first = arr[i]; // Store the first element of the pair
+                second = arr[j]; // Store the second element of the pair
+            }
         }
     }
-    if (first>second)
+
+    // Print the pair with the smallest difference in sorted order
+    if (first > second)
     {
-        printf("%d %d",second, first);
-    }
-    else if (second>first)
-    {
-        printf("%d %d",first, second);
-    }
+        printf("%d %d", second, first);
     }
     else
     {
-        printf("-1");
+        printf("%d %d", first, second);
     }
+
+    return 0;
 }
