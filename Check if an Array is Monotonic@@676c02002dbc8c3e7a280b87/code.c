fe2e6b59128dd4d53d66d411fc81accc854c38f6
vidhita8 <guptavidhita@gmail.com>
2025-03-28 01:48:32 +0530

    Solution to Check if an Array is Monotonic

--- Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
+++ Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
@@ -1,55 +1,24 @@
-// #include <stdio.h>
-// int main()
-// {
-//     int n,k=0,counter=0,rm,m;
-//     scanf("%d", &n);
-//     int arr[n];
-//     for (int i=0; i<n; i++)
-//     {
-//         scanf("%d", &arr[i]);
-//     }
-//     for (int i=0; i<n-1; i++)
-//     {
-//         if ((arr[i]>=arr[i+1]) || (arr[i]<=arr[i+1]))
-//         counter++;
-//     }
-//     if (counter==n)
-//     {
-//         printf("YES");
-//     }
-//     else
-//     printf("NO");
-// }
-
 #include <stdio.h>
-
 int main() {
-    int n;
-    // printf("Enter the number of elements: ");
+    int n, counterIncreasing = 0, counterDecreasing = 0;
     scanf("%d", &n);
-
     int arr[n];
-    // printf("Enter the elements of the array: ");
-    for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
-    int increasing = 1, decreasing = 1;
-
-    // Check if the array is non-decreasing or non-increasing
-    for (int i = 0; i < n - 1; i++) {
-        if (arr[i] < arr[i + 1]) {
-            decreasing = 0;  // If found increasing, mark decreasing as false
-        } else if (arr[i] > arr[i + 1]) {
-            increasing = 0;  // If found decreasing, mark increasing as false
+        for (int i = 0; i < n - 1; i++) {
+        if (arr[i] <= arr[i + 1]) {
+            counterIncreasing++;  
+        }
+        if (arr[i] >= arr[i + 1]) {
+            counterDecreasing++;  
         }
     }
 
-    // If either increasing or decreasing is true, it's a monotonic array
-    if (increasing || decreasing) {
-        printf("YES");
+    if (counterIncreasing == n - 1 || counterDecreasing == n - 1) {
+        printf("YES\n");  
     } else {
-        printf("NO");
+        printf("NO\n");  
     }
 
     return 0;
