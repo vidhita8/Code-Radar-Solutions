a2d7b1d1c93d4da8fe35479309e4613d537eaf30
vidhita8 <guptavidhita@gmail.com>
2025-03-28 01:40:20 +0530

    Solution to Check if an Array is Monotonic

--- Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
+++ Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
@@ -1,22 +1,56 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int n,k=0,counter=0,rm,m;
+//     scanf("%d", &n);
+//     int arr[n];
+//     for (int i=0; i<n; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
+//     for (int i=0; i<n-1; i++)
+//     {
+//         if ((arr[i]>=arr[i+1]) || (arr[i]<=arr[i+1]))
+//         counter++;
+//     }
+//     if (counter==n)
+//     {
+//         printf("YES");
+//     }
+//     else
+//     printf("NO");
+// }
+
 #include <stdio.h>
-int main()
-{
-    int n,k=0,counter=0,rm,m;
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
+
     int arr[n];
-    for (int i=0; i<n; i++)
-    {
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    for (int i=0; i<n-1; i++)
-    {
-        if ((arr[i]>=arr[i+1]) || (arr[i]<=arr[i+1]))
-        counter++;
+
+    int increasing = 1, decreasing = 1;
+
+    // Check if the array is non-decreasing or non-increasing
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] < arr[i + 1]) {
+            decreasing = 0;  // If found increasing, mark decreasing as false
+        } else if (arr[i] > arr[i + 1]) {
+            increasing = 0;  // If found decreasing, mark increasing as false
+        }
     }
-    if (counter==n)
-    {
-        printf("YES");
+
+    // If either increasing or decreasing is true, it's a monotonic array
+    if (increasing || decreasing) {
+        printf("The array is monotonic.\n");
+    } else {
+        printf("The array is not monotonic.\n");
     }
-    else
-    printf("NO");
+
+    return 0;
 }
