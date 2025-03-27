a33477fd75515b6351e7b26153d485544f01cbe5
vidhita8 <guptavidhita@gmail.com>
2025-03-28 02:32:45 +0530

    Solution to Find the Element with Maximum Frequency

--- Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
+++ Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
@@ -1,31 +1,71 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int n, k=0;
+//     scanf ("%d", &n);
+//     int arr1[n], arr2[n];
+//     for (int i=0; i<n; i++)
+//     {
+//         scanf("%d", &arr1[i]);
+//     }
+//     for (int i=0; i<n-1; i++)
+//     {k=0;
+//         for (int j=0; j<n; j++)
+//         {
+//             if (arr1[i]==arr1[j])
+//             k++;
+//         }
+//         arr2[i]=k;
+//         k=0;
+//     }
+//     int max=arr2[0], index=0;
+//     for (int i=0; i<n; i++)
+//     {
+//         if (max>arr2[i])
+//         {
+//         max=arr2[i];
+//         index=i;
+//         }
+//     }
+//     printf("%d", arr1[index]);
+// }
+
 #include <stdio.h>
-int main()
-{
-    int n, k=0;
-    scanf ("%d", &n);
+
+int main() {
+    int n, k = 0, max = 0, index = 0;
+    scanf("%d", &n);
     int arr1[n], arr2[n];
-    for (int i=0; i<n; i++)
-    {
+
+    // Input array
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr1[i]);
     }
-    for (int i=0; i<n-1; i++)
-    {k=0;
-        for (int j=0; j<n; j++)
-        {
-            if (arr1[i]==arr1[j])
-            k++;
-        }
-        arr2[i]=k;
-        k=0;
-    }
-    int max=arr2[0], index=0;
-    for (int i=0; i<n; i++)
-    {
-        if (max>arr2[i])
-        {
-        max=arr2[i];
-        index=i;
+
+    // Count occurrences of each element
+    for (int i = 0; i < n; i++) {
+        k = 1;  // Start with a count of 1 for the current element
+        if (arr2[i] != -1) {  // Skip already counted elements
+            for (int j = i + 1; j < n; j++) {
+                if (arr1[i] == arr1[j]) {
+                    k++;  // Increment the count for the duplicate elements
+                    arr2[j] = -1;  // Mark this element as counted
+                }
+            }
+            arr2[i] = k;  // Store the count for the current element
         }
     }
+
+    // Find the most frequent element
+    for (int i = 0; i < n; i++) {
+        if (arr2[i] > max) {
+            max = arr2[i];
+            index = i;
+        }
+    }
+
+    // Output the most frequent element
     printf("%d", arr1[index]);
+
+    return 0;
 }
